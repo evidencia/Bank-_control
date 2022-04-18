9d49e393bd6fc5e9d6adcf9e100a2d202effb394
evidencia <evidenciadavid2016@gmail.com>
2022-04-18 09:40:16 +0100

    commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import { Container } from "./styles";
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="entradas" />
+                </header>
+
+                <strong>R$1000,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="saidas" />
+                </header>
+
+                <strong>- R$500,00</strong>
+            </div>
+
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={total} alt="total" />
+                </header>
+
+                <strong>R$500,00</strong>
+            </div>
+        </Container>   
+    )
+}
